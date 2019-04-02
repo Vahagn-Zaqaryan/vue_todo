4a57da148905d7395ea4b2470589fab11c16a5ee
Vahagn Zaqaryan <vahagn.zaqaryan@tumo.org>
2019-04-02 19:21:24 +0400

    Added Basics

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Todos v-bind:todos="todos" v-on:del-todo="delTodo"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        Todos
+    },
+    data () {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    title: "Hello 1!",
+                    completed: false
+                },
+                {
+                    id: 2,
+                    title: "Hello 2!",
+                    completed: true
+                },
+                {
+                    id: 3,
+                    title: "Hello 3!",
+                    completed: false
+                }
+            ]
+        }
+    },
+    methods: {
+        delTodo(id) {
+            this.todos = this.todos.filter(todo => todo.id !== id)
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    body{
+        font-family: arial;
+    }
 </style>
