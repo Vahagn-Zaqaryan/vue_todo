b7dd57b821c5d92274518bb712c35d93a72bc289
Vahagn Zaqaryan <vahagn.zaqaryan@tumo.org>
2019-04-02 20:19:27 +0400

    Added to-do Add func

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,40 @@
 <template>
-    <div id="app">
-        <Todos v-bind:todos="todos" v-on:del-todo="delTodo"/>
-    </div>
+  <div id="app">
+    <Header />
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
-
+import Header from './components/layout/Header';
 export default {
-    name: 'app',
-    components: {
-        Todos
-    },
-    data () {
-        return {
-            todos: [
-                {
-                    id: 1,
-                    title: "Hello 1!",
-                    completed: false
-                },
-                {
-                    id: 2,
-                    title: "Hello 2!",
-                    completed: true
-                },
-                {
-                    id: 3,
-                    title: "Hello 3!",
-                    completed: false
-                }
-            ]
-        }
-    },
-    methods: {
-        delTodo(id) {
-            this.todos = this.todos.filter(todo => todo.id !== id)
-        }
-    }
+  name:"app",
+  components: {
+    Header
+  }
 }
 </script>
 
+
 <style>
-    *{
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-    }
-    body{
-        font-family: arial;
-    }
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
 </style>
